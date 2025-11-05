b3c479f763ec9c31c99952bc6c2864f94f7f01c5
zarielbloomp <miguel.c.nicoli@gmail.com>
2025-11-05 09:03:46 -0300

    aaaaa

--- app.py
+++ app.py
@@ -6,11 +6,14 @@ app = FastAPI()
 
 @app.get("/")
 def ola_mundo():
-    return "<p> Ola mundoa </p> <style> color: blue </style>"
+    return "<p> Ola mundoa </p>"
 
 @app.get("/teste")
 def teste():
     return{'teste': 'teste'}
+@app.get('/aluno')
+def aluno():
+    return{'nome': 'miguel', 'turma': 'imi3'}
 
 if __name__ == '__main__':
     uvicorn.run(app, host='0.0.0.0', port=8000) 
